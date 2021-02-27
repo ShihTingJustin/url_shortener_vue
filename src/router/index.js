7e3d4a8b775301d222016232c87ebdfc5542ee68
Justin Huang <justinhuang777@gmail.com>
2021-02-27 19:15:00 +0800

    vue init

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NotFound from '../views/NotFound.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound,
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
