0b0375876ffc8ca6960ac969bec2085507398249
Justin Huang <justinhuang777@gmail.com>
2021-03-12 18:30:50 +0800

    add Record component and update Manager page

---
+++ src/components/Record.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="data-wrap mt-2 border w-100">
+    <form
+      action="/"
+      method="POST"
+      class="w-100 d-flex flex-column align-items-center my-3"
+    >
+      <div class="text-left w-75">
+        <div>
+          Short URL
+          <i
+            class="copy-icon copy-link far fa-clone ml-3"
+            data-clipboard-target="#short-url"
+          ></i>
+        </div>
+        <input
+          required
+          id="short-url"
+          type="url"
+          name="url"
+          placeholder="Please enter URL here"
+          class="input-area w-100"
+          :value="shortUrl"
+        />
+      </div>
+      <div class="text-left w-75 mt-4">
+        <div>
+          Original URL
+          <i
+            class="copy-icon copy-link far fa-clone ml-3"
+            data-clipboard-target="#original-url"
+          ></i>
+        </div>
+        <input
+          required
+          id="original-url"
+          type="url"
+          name="url"
+          placeholder="Please enter URL here"
+          class="input-area w-100"
+          :value="originalUrl"
+        />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import Clipboard from "clipboard";
+
+export default {
+  name: "ShortUrl",
+  props: {
+    shortUrl: {
+      type: String,
+      require: true,
+    },
+    originalUrl: {
+      type: String,
+      require: true,
+    },
+  },
+  mounted() {
+    this.initClipboard();
+  },
+  methods: {
+    initClipboard() {
+      const clipboard = new Clipboard(".copy-link");
+      clipboard.on("success", () => {
+        console.log("copy successfully");
+      });
+      clipboard.on("error", () => {
+        console.log("copy fail");
+      });
+    },
+  },
+};
+</script>
--- src/views/Manager.vue
+++ src/views/Manager.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>manager page</h1>
+  <div class="container d-flex flex-column align-items-center">
+    <div class="logo w-100 text-center">
+      <a href="/">URL SHORTENER<i class="fas fa-link fa-lg ml-2"></i></a>
+    </div>
+    <Record />
+    <a
+      href="/"
+      class="submit-btn btn w-50"
+      style="text-decoration: none; color: #ececec"
+      >Create another</a
+    >
+  </div>
 </template>
+
+<script>
+import Record from "./../components/Record";
+
+export default {
+  name: "Manager",
+  components: {
+    Record,
+  },
+  created() {
+
+  },
+  methods: {
+    fetchRecords() {
+      
+    }
+  }
+};
+</script>
